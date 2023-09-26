13c22cf5a21f2e8b7bb3f7aec65fe5b97bee67c1
taylorbrook <taylor.brook@gmail.com>
2023-09-26 14:53:05 -0700

    multiple tags

--- simple tag search/script.js
+++ simple tag search/script.js
@@ -1,35 +1,45 @@
+const tagSelect = document.getElementById('tagSelect');
+const filterButton = document.getElementById('filterButton');
+
 // Load data from data.json
+let data = []; // Store the loaded data globally
+
 fetch('data.json')
     .then(response => response.json())
-    .then(data => {
-        // Populate tag options
-        const tagSelect = document.getElementById('tagSelect');
-        const tags = new Set();
+    .then(jsonData => {
+        data = jsonData; // Store the loaded data
+        populateTagOptions();
+    });
 
-        data.forEach(item => {
-            item.tags.forEach(tag => {
-                tags.add(tag);
-            });
-        });
+function populateTagOptions() {
+    const tags = new Set();
 
-        tags.forEach(tag => {
-            const option = document.createElement('option');
-            option.text = tag;
-            tagSelect.appendChild(option);
-        });
-
-        // Filter and display items based on tag selection
-        tagSelect.addEventListener('change', () => {
-            const selectedTag = tagSelect.value;
-            const itemContainer = document.getElementById('itemContainer');
-            itemContainer.innerHTML = '';
-
-            data.forEach(item => {
-                if (item.tags.includes(selectedTag) || selectedTag === 'All') {
-                    const itemDiv = document.createElement('div');
-                    itemDiv.textContent = item.name;
-                    itemContainer.appendChild(itemDiv);
-                }
-            });
+    data.forEach(item => {
+        item.tags.forEach(tag => {
+            tags.add(tag);
         });
     });
+
+    const tagSelect = document.getElementById('tagSelect');
+
+    tags.forEach(tag => {
+        const option = document.createElement('option');
+        option.text = tag;
+        option.value = tag;
+        tagSelect.appendChild(option);
+    });
+}
+
+filterButton.addEventListener('click', () => {
+    const selectedTags = Array.from(tagSelect.selectedOptions, option => option.value);
+    const itemContainer = document.getElementById('itemContainer');
+    itemContainer.innerHTML = '';
+
+    data.forEach(item => {
+        if (selectedTags.every(tag => item.tags.includes(tag)) || selectedTags.length === 0) {
+            const itemDiv = document.createElement('div');
+            itemDiv.textContent = item.name;
+            itemContainer.appendChild(itemDiv);
+        }
+    });
+});
